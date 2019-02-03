f73f69781ba9e3f9d4c26a53c016305d4a02bb61
Heather Smith <heather@Heathers-iMac.local>
2019-02-03 12:59:42 -0800

    Adds to Address Model

--- lib/address.rb
+++ lib/address.rb
@@ -1,16 +1,29 @@
 require_relative 'geocoding'
 
+
 class Address
-  @@list = []
+  geocoded_by :address       
+  after_validation :geocode
 
   attr_accessor :lat, :lng, :full_address
 
-  def initialize (lat lng full_address=nil)
+  def initialize (lat, lng, full_address=nil)
     @lat = lat
     @lng = lng
-    @full_address = full_address
+    @full_address = full_address || find_address(:lat, :lng)
   end
 
+  # def self.all
+  #   address = []
+  #   @@list.each do |coordinates|
+  #     Address.new(coordinates.lat, coordinates.lng)
+  #   end
+  #   address
+  # end
 
+  def find_address(lattitude, longitude)
+    reverse_geocoded_address = Geocoder.reverse_geocoded_by :lattitude, :longitude
+    puts reverse_geocoded_address
+  end
 
 end
--- main.rb
+++ main.rb
@@ -3,6 +3,27 @@ Dir['./lib/*.rb'].each { |f| require f }
 
 class Main < Sinatra::Base
   get '/' do
+
+    @list = [
+        {lat: 61.582195, lng: -149.443512},
+        {lat: 44.775211, lng: -68.774184},
+        {lat: 25.891297, lng: -97.393349},
+        {lat: 45.787839, lng: -108.502110},
+        {lat: 35.109937, lng: -89.959983}
+    ]
+
+    @address_list = []
+
+    @list.each do | coordinate |
+        @address_list.push(Address.new(coordinate[:lat], coordinate[:lng]))
+    end
+
     erb :index #, locals: { address: address }
   end
+
+  # post '/address' do
+  #   require 'pry'; binding.pry
+  #   params.to_s
+  # end
+
 end
