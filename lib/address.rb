d406fbc867b4f9ddbca0dc94c857a2857b08f9ce
Heather Smith <heather@Heathers-iMac.local>
2019-02-03 10:18:39 -0800

    Adds Form Lat/Lng Submit

--- lib/address.rb
+++ lib/address.rb
@@ -1,5 +1,16 @@
 require_relative 'geocoding'
 
 class Address
+  @@list = []
+
   attr_accessor :lat, :lng, :full_address
+
+  def initialize (lat lng full_address=nil)
+    @lat = lat
+    @lng = lng
+    @full_address = full_address
+  end
+
+
+
 end
