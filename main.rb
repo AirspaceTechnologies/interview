fdaa276a9ee4ac58395af3d15055f1d0919852f3
Mike LaRocca <michaellarocca90>
2019-02-05 13:39:25 -0800

    Styling

--- main.rb
+++ main.rb
@@ -3,9 +3,9 @@ Dir['./lib/*.rb'].each { |f| require f }
 
 class Main < Sinatra::Base
   get '/' do
-    @full_address = "1600 Pennsylvania Avenue NW Washington, D.C. 20500 U.S."
+    @dc_address = "1600 Pennsylvania Avenue NW Washington, D.C. 20500 U.S."
     @coordinate_list = [
-        {lat: 61.582195, lng: -149.443512},
+        {lat: 61.582195, lng: -149.443512, full_address: "1600 Pennsylvania Avenue NW Washington, D.C. 20500 U.S."},
         {lat: 44.775211, lng: -68.774184},
         {lat: 25.891297, lng: -97.393349},
         {lat: 45.787839, lng: -108.502110},
@@ -20,7 +20,7 @@ class Main < Sinatra::Base
     #     @address_list.push(address)
     # end
 
-    erb :index #, locals: { address: address }
+    erb :index2 #, locals: { address: address }
   end
 
 
